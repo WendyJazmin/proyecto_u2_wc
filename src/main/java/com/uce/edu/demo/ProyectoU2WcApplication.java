fbc0b9429e25eaa43ba4fe4fb45fa0561fd0406e
wendyjazmin <wendyjazmin1994@hotmail.com>
2022-06-27 20:24:19 -0500

    taller15

--- src/main/java/com/uce/edu/demo/ProyectoU2WcApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2WcApplication.java
@@ -1,13 +1,20 @@
 package com.uce.edu.demo;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.demo.service.IPersonaJdbcService;
+import com.uce.edu.demo.to.Persona;
+
 @SpringBootApplication
 public class ProyectoU2WcApplication implements CommandLineRunner{
 
+	//@Autowired
+	private IPersonaJdbcService iPersonaJdbcService;
+	
 	private static Logger LogJava = Logger.getLogger(ProyectoU2WcApplication.class);
 
 	
@@ -19,7 +26,24 @@ public class ProyectoU2WcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
-		LogJava.debug("HELLO");
+		
+		Persona persona = new Persona();
+		persona.setId(4);
+		persona.setNombre("Daniel");
+		persona.setApellido("Satur");
+		System.out.println(persona.getApellido());;
+		
+		//Insertar
+		this.iPersonaJdbcService.guardar(persona);
+		
+		Persona per1 = new Persona();
+		per1.setId(1);
+		per1.setNombre("Andres");
+		per1.setApellido("Fare");
+		
+		//actualizar
+		//this.iPersonaJdbcService.actualizar(per1);
+		//this.iPersonaJdbcService.eliminar(2);
 	}
 
 }
--- src/main/java/com/uce/edu/demo/to/Persona.java
+++ src/main/java/com/uce/edu/demo/to/Persona.java
@@ -2,6 +2,33 @@ package com.uce.edu.demo.to;
 
 public class Persona {
 
+	private int id;
+	private String nombre;
+	private String apellido;
+	
+	
+
+	//set y get
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 	
 	
 }
